317fb34a4a4c9f3d1214b75b7cf333e8caa5e8c4
Fernando-Goncalves-Campos <nandito3990@gmail.com>
2023-04-17 20:17:18 -0300

    Melhoria do código

--- lojaonline/src/Layout.js
+++ lojaonline/src/Layout.js
@@ -1,12 +1,12 @@
 import { useState, memo } from "react";
 import { Outlet, Link } from "react-router-dom";
-import './App.css';
+import './Layout.css';
+import './Theme.css';
 
 function Layout() {
     const [theme, setTheme] = useState("dark-theme")
 
     // function to toggle between light and dark theme
-    
     function toggleTheme() {
         if (theme === 'dark-theme') {
             setTheme('light-theme');
@@ -17,16 +17,24 @@ function Layout() {
     }
 
     return (
-        <><div id="container" className={theme}>
+        <><div className={theme}>
             <nav id="navBar">
                 <button id="logo">Logo</button>
-                <button id="userNav">Login</button>
+                
+                <div id="search">
+                    <input type="text" id="searchText"></input>
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </div>
                 <button id="switchTheme" onClick={() => toggleTheme()}><i className="fa-solid fa-circle-half-stroke"></i></button>
-
+                <button id="userNav">Login</button>
             </nav>
-        </div>
-
-        <Outlet /></>
+        
+            <div id="content">
+                <div style={{backgroundImage: "linear-gradient(180deg, rgba(0,76,167,1) 0%, rgba(0,0,1,1) 100%)", height: "5000px"}}></div>
+            
+                <Outlet />
+            </div>
+        </div></>
   );
 }
 
