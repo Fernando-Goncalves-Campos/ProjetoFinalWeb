2b245c4e4b0601b84fe994f2d8003248aa11f59c
joseccarmo <carmoj272@gmail.com>
2023-04-25 15:19:03 -0300

    carrinho adicionado

--- lojaonline/src/Store/ItemOption.js
+++ lojaonline/src/Store/ItemOption.js
@@ -1,27 +1,26 @@
 import { memo } from "react";
 import { useNavigate } from "react-router-dom";
-import './Store.css';
+import "./Store.css";
 
-function ItemOption({props, item}) {
-    //Usado para redirecionar o usuário para outra rota do site
-    const navigate = useNavigate();
-    
-    //Para a tela com o restante dos dados do item, enviando o ID do item no URL
-    function handleClick(){
-        navigate(`/itemDetails/${item.id}`);
-    }
-    
-    return (
-        <div className="storeItem" onClick={handleClick}>
-            <img src={item.photo} alt={item.name} /> <br />
-            <div class="storeItemText">
-                <span className="itemName"> {item.name} </span> <br/>
-                <span className="itemId"> {item.id} </span> <br/>
-                <span className="itemPrice"> {item.price} </span> <br/>
-            </div>
-        </div>
-    );
+function ItemOption({ props, item }) {
+	//Usado para redirecionar o usuário para outra rota do site
+	const navigate = useNavigate();
+
+	//Para a tela com o restante dos dados do item, enviando o ID do item no URL
+	function handleClick() {
+		navigate(`/itemDetails/${item.id}`);
+	}
+
+	return (
+		<div className="storeItem" onClick={handleClick}>
+			<img src={item.photo} alt={item.name} /> <br />
+			<div class="storeItemText">
+				<span className="itemName"> {item.name} </span> <br />
+				<span className="itemId"> {item.id} </span> <br />
+				<span className="itemPrice"> {item.price} </span> <br />
+			</div>
+		</div>
+	);
 }
-
 
 export default memo(ItemOption);
